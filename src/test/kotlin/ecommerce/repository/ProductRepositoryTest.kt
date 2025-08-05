9ed0708517d5aaa93611bfe5b09c04f7de127b63
arekiu <alexis.aguilera@deliveryhero.com>
2025-08-05 17:24:38 +0200

    feat(ProductRepositoryTest): add tests to check options in product

--- src/test/kotlin/ecommerce/repository/ProductRepositoryTest.kt
+++ src/test/kotlin/ecommerce/repository/ProductRepositoryTest.kt
@@ -20,6 +20,9 @@ class ProductRepositoryTest {
         assertThat(actual.id).isNotNull
         assertThat(actual.id).isNotZero
         assertThat(actual.name).isEqualTo(expected.name)
+        assertThat(actual.options.first().name).isEqualTo("test")
+        assertThat(actual.options.first().quantity).isEqualTo(10)
+
     }
 
     @Test
