aaf615fdd18e8e1e54ad7e9d0a8162a16f9ffb0c
arekiu <alexis.aguilera@deliveryhero.com>
2025-08-09 18:01:02 +0200

    fix(CartController): delete unnecessary line

--- src/main/kotlin/ecommerce/controller/CartController.kt
+++ src/main/kotlin/ecommerce/controller/CartController.kt
@@ -27,7 +27,6 @@ class CartController(
         @RequestBody request: CartItemRequest,
         @LoginMember member: MemberDto,
     ): ResponseEntity<CartItemResponse> {
-        cartService.addOrUpdateCartItem(member.id, request)
         val addToCartResult = cartService.addOrUpdateCartItem(member.id, request)
         return ResponseEntity.ok(addToCartResult)
     }
