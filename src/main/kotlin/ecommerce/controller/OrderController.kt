4e8dc4bc5face6455582fc8437f5cccc1bdab194
arekiu <alexis.aguilera@deliveryhero.com>
2025-08-11 15:52:33 +0200

    feat(OrderController): add endpoint for getOrderStatus

--- src/main/kotlin/ecommerce/controller/OrderController.kt
+++ src/main/kotlin/ecommerce/controller/OrderController.kt
@@ -3,10 +3,13 @@ package ecommerce.controller
 import ecommerce.annotation.LoginMember
 import ecommerce.dto.MemberDto
 import ecommerce.dto.OrderResponse
+import ecommerce.dto.OrderStatusResponse
 import ecommerce.dto.PaymentRequest
 import ecommerce.mapper.toResponse
 import ecommerce.service.OrderService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -24,5 +27,14 @@ class OrderController(
     ): ResponseEntity<OrderResponse> {
         val order = orderService.placeOrder(member.id, paymentRequest)
         return ResponseEntity.ok(order.toResponse())
+    }
+
+    @GetMapping("/{orderId}/status")
+    fun getOrderStatus(
+        @LoginMember member: MemberDto,
+        @PathVariable orderId: Long,
+    ): ResponseEntity<OrderStatusResponse> {
+        val orderStatus = orderService.getOrderStatus(orderId, member.id)
+        return ResponseEntity.ok(orderStatus)
     }
 }
