61a9469e57c7c860f5717aa4d5540705b9edf030
arekiu <alexis.aguilera@deliveryhero.com>
2025-08-13 21:31:09 +0200

    fix(CartController): remove duplicated function call

--- src/main/kotlin/ecommerce/controller/CartController.kt
+++ src/main/kotlin/ecommerce/controller/CartController.kt
@@ -56,7 +56,6 @@ class CartController(
         @RequestBody request: CartItemRequest,
         @LoginMember member: MemberDto,
     ): ResponseEntity<CartItemResponse?> {
-        cartService.addOrUpdateCartItem(member.id, request)
         val addToCartResult = cartService.addOrUpdateCartItem(member.id, request)
         return ResponseEntity.ok(addToCartResult)
     }
